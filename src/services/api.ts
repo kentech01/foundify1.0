2a07bea9e5fa27ed7c26ffe3497cdbe3f9ab9ca4
SkenderAdemi <skender.ad@gmail.com>
2025-11-17 19:37:24 +0100

    changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,9 +3,7 @@ import { useCallback } from "react";
 import useAxios from "../hooks/useAxios";
 import { log } from "util";
 import { data } from "react-router-dom";
-// export const API_BASE_URL =
-// "https://foundify-api-production.up.railway.app/api/v1/";
-export const API_BASE_URL = "http://localhost:5001/api/v1/";
+export const API_BASE_URL = "https://foundify-api-production.up.railway.app/api/v1/";
 
 interface ApiResponse<T> {
   success: boolean;
