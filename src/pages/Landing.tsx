e29f964a0ad4cae090de52bee54b2eb52f98df97
Erion <hysenierion8@gmail.com>
2026-02-16 16:05:14 +0100

    credibility removed, landing page guard redirect

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Nav } from "@/components/landingpage/Nav";
 import { Hero } from "@/components/landingpage/Hero";
@@ -14,15 +14,24 @@ import SignInModal from "@/components/signIn/SignInModal";
 
 export default function Landing() {
   const navigate = useNavigate();
-  const { user } = UserAuth();
+  const { user, loading } = UserAuth();
   const [isSignInModalOpen, setIsSignInModalOpen] = useState(false);
 
   // Handler for "Get Started" buttons - goes to builder if not logged in, dashboard if logged in
+  useEffect(()=>{
+    if(!loading){
+      if (user) {
+        navigate("/dashboard");
+      }
+     }
+    
+  }, [loading])
   const handleGetStarted = () => {
+    
     if (user) {
       navigate("/dashboard");
-    } else {
-      navigate("/builder");
+    }else{
+      navigate("/builder")
     }
   };
 
@@ -36,25 +45,25 @@ export default function Landing() {
   };
 
   return (
-    <div className="min-h-screen bg-white font-sans selection:bg-indigo-50 selection:text-indigo-600">
-      <Nav onDashboardClick={handleDashboardClick} onSignInClick={() => setIsSignInModalOpen(true)} />
-      <main>
-        <Hero onGetStarted={handleGetStarted} />
-        {/* <LogoMarquee /> */}
-        <HowItWorks />
-        <AiWorkflows onGetStarted={handleGetStarted} />
-        <Pricing onGetStarted={handleGetStarted} />
-        <WhyFoundify onGetStarted={handleGetStarted} />
-      </main>
-      <Footer />
-      <SignInModal
-        isOpen={isSignInModalOpen}
-        onClose={() => setIsSignInModalOpen(false)}
-        onSignInSuccess={() => {
-          setIsSignInModalOpen(false);
-          navigate("/dashboard");
-        }}
-      />
-    </div>
+    ((!loading && !user) && <div className="min-h-screen bg-white font-sans selection:bg-indigo-50 selection:text-indigo-600">
+    <Nav onDashboardClick={handleDashboardClick} onSignInClick={() => setIsSignInModalOpen(true)} />
+    <main>
+      <Hero onGetStarted={handleGetStarted} />
+      {/* <LogoMarquee /> */}
+      <HowItWorks />
+      <AiWorkflows onGetStarted={handleGetStarted} />
+      <Pricing onGetStarted={handleGetStarted} />
+      <WhyFoundify onGetStarted={handleGetStarted} />
+    </main>
+    <Footer />
+    <SignInModal
+      isOpen={isSignInModalOpen}
+      onClose={() => setIsSignInModalOpen(false)}
+      onSignInSuccess={() => {
+        setIsSignInModalOpen(false);
+        navigate("/dashboard");
+      }}
+    />
+  </div>)
   );
 }
